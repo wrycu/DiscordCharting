7c796c31df01c300348e609e996de0fb67209d8a
Wrycu <blah380@gmail.com>
2016-03-16 20:59:51 -0400

    fix(import) correct imports under wsgi not loading properly

--- app/flask_app/__init__.py
+++ app/flask_app/__init__.py
@@ -3,7 +3,7 @@ from app.flask_app.discord_charting.controllers import discord_charting
 from flask_cors import CORS
 
 app = Flask(__name__)
-app.config.from_object('config')
+app.config.from_object('app.config')
 cors = CORS(app)
 
 
